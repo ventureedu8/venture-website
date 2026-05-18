590a9f45d5fb5ef4af7ea0e6f277a87ef9921757
Jennifer <venture.edu8@gmail.com>
2026-05-19 00:50:31 +0800

    修正白畫面與更新 Slogan

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 關鍵在這裡！必須告訴系統您的網站在這個資料夾底下：
+  base: '/venture-website/', 
 })
